2183ab44a8e3a63679c1092e7a8623661965b1fd
liutaoluke <liutao_luke@hotmail.com>
2024-07-08 10:21:29 +0800

    V02-01 : add destructor - QModbusReply & QModbusDataUnit
        void testQModbusReply();
        void testQModbusDataUnit();

--- destructor.h
+++ destructor.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <QDebug>
+#include <QModbusDataUnit>
+#include <QModbusReply>
 #include <QObject>
+#include <type_traits>
 
 class Destructor : public QObject {
     Q_OBJECT
 public:
     explicit Destructor(QObject *parent = nullptr);
+
+    void testQModbusReply();
+    void testQModbusDataUnit();
 
 signals:
 };
