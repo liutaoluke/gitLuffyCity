8c17612d46bbd50aee1d3b3b1ac170c40443f696
liutaoluke <liutao_luke@hotmail.com>
2024-07-08 10:35:45 +0800

    V01-05 : fixbug - modify Debug()
    from QModbusReply to QModbusDataUnit

--- constructorassignment.cpp
+++ constructorassignment.cpp
@@ -33,8 +33,8 @@ void ConstructorAssignment::testQModbusDataUnit() {
     qDebug() << "QModbusDataUnit copy constructible: " << isCopyConstructible;
     qDebug() << "QModbusDataUnit copy assignable: " << isCopyAssignable;
 
-    qDebug() << "QModbusReply move constructible: " << isMoveConstructible;
-    qDebug() << "QModbusReply move assignable: " << isMoveAssignable;
+    qDebug() << "QModbusDataUnit move constructible: " << isMoveConstructible;
+    qDebug() << "QModbusDataUnit move assignable: " << isMoveAssignable;
 }
 
 void ConstructorAssignment::testQVector() {
